512b3e6c5399c77bff192a42cd518830e87ba92a
“Migakin <kinga.miga@gmail.com>
2018-01-18 13:44:12 +0100

    19.1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const apiRoot = 'https://ancient-caverns-77757.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://ancient-caverns-77757.herokuapp.com//v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
